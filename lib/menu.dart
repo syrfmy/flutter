a4f9b07c39604404edf4b5cbc5e9422b76787a9c
syrfmy <syariefmulyadi24@gmail.com>
2023-11-08 16:16:16 +0700

    Tutorial 2

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/shoplist_form.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
   final List<ShopItem> items = [
-    ShopItem("Lihat Produk", Icons.checklist),
-    ShopItem("Tambah Produk", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+    ShopItem("Lihat Item", Icons.checklist, Colors.red),
+    ShopItem("Tambah Item", Icons.add_shopping_cart, Colors.green),
+    ShopItem("Logout", Icons.logout, Colors.blue),
   ];
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Shopping List',
+          'Shopping Lists',
         ),
+        backgroundColor: Colors.indigo,
+        foregroundColor: Colors.white,
       ),
+      drawer: const LeftDrawer(),
       body: SingleChildScrollView(
         // Widget wrapper yang dapat discroll
         child: Padding(
@@ -28,7 +31,7 @@ class MyHomePage extends StatelessWidget {
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
                 child: Text(
-                  'PBP Shop', // Text yang menandakan toko
+                  'What to do?', // Text yang menandakan toko
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 30,
@@ -47,7 +50,7 @@ class MyHomePage extends StatelessWidget {
                 shrinkWrap: true,
                 children: items.map((ShopItem item) {
                   // Iterasi untuk setiap item
-                  return ShopCard(item);
+                  return ShopCard(item, item.cardColor);
                 }).toList(),
               ),
             ],
@@ -56,35 +59,36 @@ class MyHomePage extends StatelessWidget {
       ),
     );
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    // This widget is the home page of your application. It is stateful, meaning
+    // that it has a State object (defined below) that contains fields that affect
+    // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    // This class is the configuration for the state. It holds the values (in this
+    // case the title) provided by the parent (in this case the App widget) and
+    // used by the build method of the State. Fields in a Widget subclass are
+    // always marked "final".
 
 
-    }
+  }
 }
 
 class ShopItem {
   final String name;
   final IconData icon;
-
-  ShopItem(this.name, this.icon);
+  final Color cardColor;
+  ShopItem(this.name, this.icon, this.cardColor);
 }
 
 class ShopCard extends StatelessWidget {
   final ShopItem item;
+  final Color cardColor;
 
-  const ShopCard(this.item, {super.key}); // Constructor
+  const ShopCard(this.item, this.cardColor ,{super.key}); // Constructor
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.indigo,
+      color: item.cardColor,
       child: InkWell(
         // Area responsive terhadap sentuhan
         onTap: () {
@@ -93,7 +97,11 @@ class ShopCard extends StatelessWidget {
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
-        },
+            if (item.name == "Tambah Produk") {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const ShopFormPage()));
+            }
+          },
         child: Container(
           // Container untuk menyimpan Icon dan Text
           padding: const EdgeInsets.all(8),
